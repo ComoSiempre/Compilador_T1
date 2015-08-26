9e5754408aae0cf767fadc4439302eb97dfbb4ff
ComoSiempre <jvasquezr99@gmail.com>
2015-08-26 17:02:29 -0300

    Update Compilador.java

--- Compilador/src/compilador/Compilador.java
+++ Compilador/src/compilador/Compilador.java
@@ -16,7 +16,7 @@ public class Compilador {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.print("HOla mundo");
+        System.out.print("Hola mundo lokillo");
     }
     
 }
